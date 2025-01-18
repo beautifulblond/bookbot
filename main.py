6f70a8649f5c46d4795221c48031abb0479fb92d
beautifulblond <ryanalholder@gmail.com>
2025-01-17 18:16:19 -0800

    added word count

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
+    '''with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    print(file_contents)'''
+    word_count()
+
+def word_count():
+    word_count = 0
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
-
+    words = file_contents.split()
+    for word in words:
+        word_count += 1
+    print(f"There are {word_count} words in frankenstein.txt!")
+    
 main()
