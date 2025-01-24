5f7b39f856b98b0e18d37733913fbc2f2fda432e
beautifulblond <ryanalholder@gmail.com>
2025-01-24 14:33:14 -0800

    added character dictionary

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
         file_contents = f.read()
     print(file_contents)'''
     word_count()
+    char_count()
 
 def word_count():
     word_count = 0
@@ -12,5 +13,18 @@ def word_count():
     for word in words:
         word_count += 1
     print(f"There are {word_count} words in frankenstein.txt!")
+
+def char_count():
+    char_ls = []
+    char_dict = {}
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    file_contents = file_contents.lower()
+    for i in file_contents:
+        if i not in char_dict:
+            char_dict[i] = 1
+        elif i in char_dict:
+            char_dict[i] += 1
+    print(char_dict)
     
 main()
